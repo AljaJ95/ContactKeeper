bebbe45e017afa53987077a116ccb6ce8f2def83
AljaJ95 <aljastantar@gmail.com>
2022-08-04 17:07:06 +0200

    firstCommit

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//Connect database
+connectDB();
+
+//Init middleware
+app.use(express.json()); 
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...' }));
 
 //Define Routes
 app.use('/api/users', require('./routes/users.js'));
 app.use('/api/auth', require('./routes/auth.js'));
-app.use('/api/contact', require('./routes/contacts.js'));
+app.use('/api/contacts', require('./routes/contacts.js'));
 
 const PORT = process.env.PORT || 5000;
 
